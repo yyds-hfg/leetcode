516fd0b24feb4c4048d6311ba94ce4014151f693
hfg-yyds <1841814080@qq.com>
2022-06-21 13:22:01 +0800

    两数之和-1-使用HashMap,以达到空间换时间

--- src/main/java/leetcode/editor/cn/TwoSum.java
+++ src/main/java/leetcode/editor/cn/TwoSum.java
@@ -1,5 +1,7 @@
 package leetcode.editor.cn;
 
+import java.util.HashMap;
+
 /**
  * 题目名字---:两数之和
  * 题目编号---:1
@@ -11,16 +13,25 @@ public class TwoSum {
         Solution solution = new TwoSum().new Solution();
         solution.twoSum(null, 3);
     }
-
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] + nums[i] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] + nums[i] == target) {
-                        return new int[]{i, j};
-                    }
+                if (hashMap.containsKey(target-nums[i])) {
+                    return new int[]{hashMap.get(target-nums[i]),i};
                 }
+                hashMap.put(nums[i],i);
             }
             return null;
         }
