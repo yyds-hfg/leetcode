2276112fee9894164c68aeefa4fc909c03b726fe
hfg-yyds <1841814080@qq.com>
2022-06-28 22:25:30 +0800

    合并两个有序链表-21-

---
+++ src/main/java/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,50 @@
+package leetcode.editor.cn;
+
+/**
+ * 题目名字---:合并两个有序链表
+ * 题目编号---:21
+ * 解题时间---:2022-06-28 22:22:46
+ */
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            return null;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
