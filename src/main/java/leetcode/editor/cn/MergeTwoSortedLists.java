b53399404407f454433e8fbdc1bbcaef88d268d6
hfg-yyds <1841814080@qq.com>
2022-06-28 23:51:39 +0800

    合并两个有序链表-21-递归

--- src/main/java/leetcode/editor/cn/MergeTwoSortedLists.java
+++ src/main/java/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -11,6 +11,31 @@ public class MergeTwoSortedLists {
         Solution solution = new MergeTwoSortedLists().new Solution();
     }
 
+    //迭代法
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        //定义一个哨兵节点
+        ListNode curr = new ListNode(-1);
+        ListNode pre = curr;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                pre.next = list1;
+                pre = list1;
+                list1 = list1.next;
+            } else {
+                pre.next = list2;
+                pre = list2;
+                list2 = list2.next;
+            }
+        }
+        pre.next = (list1 == null ? list2 : list1);
+        return curr.next;
+    }
 //leetcode submit region begin(Prohibit modification and deletion)
 
     /**
@@ -26,28 +51,15 @@ public class MergeTwoSortedLists {
     class Solution {
         //迭代法
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            if (list1 == null) {
+            if (list1 == null) return list2;
+            else if (list2 == null) return list1;
+            if (list1.val <= list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
                 return list2;
             }
-            if (list2 == null) {
-                return list1;
-            }
-            //定义一个哨兵节点
-            ListNode curr = new ListNode(-1);
-            ListNode pre = curr;
-            while (list1 != null && list2 != null) {
-                if (list1.val < list2.val) {
-                    pre.next = list1;
-                    pre = list1;
-                    list1 = list1.next;
-                } else {
-                    pre.next = list2;
-                    pre = list2;
-                    list2 = list2.next;
-                }
-            }
-            pre.next = (list1 == null ? list2 : list1);
-            return curr.next;
         }
     }
 
