c274432f7b692f268472b72db8968929f362f5f2
hfg-yyds <1841814080@qq.com>
2022-07-08 00:11:28 +0800

    最长连续序列-128-暴力解决办法但是超时-小优化

--- src/main/java/leetcode/editor/cn/LongestConsecutiveSequence.java
+++ src/main/java/leetcode/editor/cn/LongestConsecutiveSequence.java
@@ -1,7 +1,5 @@
 package leetcode.editor.cn;
 
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -16,29 +14,33 @@ public class LongestConsecutiveSequence{
         Solution solution = new LongestConsecutiveSequence().new Solution();
         System.out.println(solution.longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
     }
-    
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums == null || nums.length==0) {
             return 0;
         }
-        HashSet<Integer> hashSet = new HashSet<>();
+        //排序并去重
         nums = Arrays.stream(nums).sorted().distinct().toArray();
         int count =1;
+        int maxLength = count;
         for (int num : nums) {
+            //判断是否存在后继元素
             if (isSuffixNum(num,nums)) {
                 count++;
             } else {
-                hashSet.add(count);
+                maxLength = Math.max(maxLength, count);
                 count = 1;
             }
         }
-        return hashSet.stream().max(Integer::compareTo).get();
+        //
+        return maxLength;
     }
     public boolean isSuffixNum(Integer num,int[] nums) {
         return Arrays.stream(nums).anyMatch(x -> x==num+1);
     }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
