ac29686b4745a63e54078f7f80d20f8700c90374
hfg-yyds <1841814080@qq.com>
2022-07-09 22:25:35 +0800

    LRU 缓存-146-通过继承LinkedHashMap实现最近最少使用缓存

---
+++ src/test/java/com/hacker/TestClas.java
@@ -0,0 +1,21 @@
+package com.hacker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
+/**
+ * @Author: Zero
+ * @Date: 2022/7/9 22:22
+ * @Description:
+ */
+public class TestClas {
+
+    @Test
+    public void testPriorityQueue() {
+        PriorityQueue priorityQueue = new PriorityQueue();
+        TreeSet<String> treeSet = new TreeSet<>();
+
+    }
+}
